bee091379a2e1ff91e2d3d82795578862c5a79f7
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-10-02 01:18:57 +0700

    feat: desain pattern src client side

---
+++ frontend/src/components/button/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function NButton({
+  children,
+  action,
+  variant,
+  size,
+  loading,
+  disabled,
+  className,
+}) {
+  return (
+    <Button
+      className={className}
+      onClick={action}
+      variant={variant}
+      disabled={disabled}
+      size={size}
+    >
+      {loading ? "Loading..." : children}
+    </Button>
+  );
+}
+
+export default NButton;
