b8c5d2d81f95f20b5246cf507ef4f36bd45e3d36
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-10-14 23:54:13 +0700

    feat: add breadcrumb component

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import SBreadCrumb from "../../components/Breadcrumb";
 
-export default function PageDashboard() {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    return <div>Please login to access the dashboard.</div>;
-  }
+export default function Dashboard() {
   return (
-    <div>
-      <h1>dashboard</h1>
-    </div>
+    <Container className="mt-3">
+      <SBreadCrumb />
+      <h1>Dashboard</h1>
+    </Container>
   );
 }
