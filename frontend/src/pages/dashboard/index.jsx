ef319464fe030181a4b342811e875cc5406ac175
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-10-20 23:53:59 +0700

    feat: add navbar on dashboard

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import SBreadCrumb from "../../components/Breadcrumb";
+import SNavbar from "../../components/Navbar";
 
 export default function Dashboard() {
   return (
     <Container className="mt-3">
+      <SNavbar></SNavbar>
       <SBreadCrumb />
       <h1>Dashboard</h1>
     </Container>
